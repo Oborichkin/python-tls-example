50d01804cdfbe3ce86f9d706dc15111bb6f711c0
Pavel Oborin <oborin.p@gmail.com>
2019-05-16 16:07:38 +0300

    :tada: Initial commit

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import ssl
+
+from server import HOST as SERVER_HOST
+from server import PORT as SERVER_PORT
+
+HOST = "127.0.0.1"
+PORT = 60002
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+client = ssl.wrap_socket(client, keyfile="key.pem", certfile="cert.pem")
+
+if __name__ == "__main__":
+    print(f"Starting client on {HOST}:{PORT}")
+    client.bind((HOST, PORT))
+    client.connect((SERVER_HOST, SERVER_PORT))
+    while True:
+        from time import sleep
+
+        client.send("Hello World!".encode("utf-8"))
+        sleep(1)
+
---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+import ssl
+
+HOST = "127.0.0.1"
+PORT = 60000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server = ssl.wrap_socket(
+    server, server_side=True, keyfile="server_key.pem", certfile="server_cert.pem"
+)
+
+if __name__ == "__main__":
+    from time import sleep
+    import select
+
+    print(f"Serving server on {HOST}:{PORT}")
+    server.bind((HOST, PORT))
+    server.listen(1)
+    while True:
+        print("Waiting for connections")
+        connection, client_address = server.accept()
+        print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+        while True:
+            r, w, x = select.select([connection], [connection], [connection])
+            if r:
+                data = r[0].recv(1024)
+                if not data:
+                    break
+                print(f"Received {data.decode('utf-8')}")
